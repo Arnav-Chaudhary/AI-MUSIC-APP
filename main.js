527aa568afc07b9bbe735ee74183fddffe2253f5
Arnav-Chaudhary <71707268+Arnav-Chaudhary@users.noreply.github.com>
2021-01-14 01:06:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,85 @@
+HarryPotter = "";
+PeterPan = "";	
+Harry_status = "";
+Peter_status = "";
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+rightWristX = 0;
+rightWristY = 0;
+leftWristX = 0;
+leftWristY = 0;
+
+function preload()
+{
+    HarryPotter = loadSound("music.mp3");
+	PeterPan = loadSound("music2.mp3");
+}
+
+function setup() {
+    canvas= createCanvas(600,500);
+    canvas.position(300,200);
+    video= createCapture(VIDEO);
+    video.hide();
+    posNet= ml5.poseNet(video,modelLoaded);
+    posNet.on('pose',gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+	console.log(results);
+	scoreRightWrist =  results[0].pose.keypoints[10].score;
+	scoreLeftWrist =  results[0].pose.keypoints[9].score;
+	console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
+	
+	rightWristX = results[0].pose.rightWrist.x;
+	rightWristY = results[0].pose.rightWrist.y;
+	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+
+	leftWristX = results[0].pose.leftWrist.x;
+	leftWristY = results[0].pose.leftWrist.y;
+	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+		
+  }
+}
+
+function draw() {
+	image(video, 0, 0, 600, 500);
+	
+	Harry_status = HarryPotter.isPlaying();
+	Peter_status = PeterPan.isPlaying();
+
+	fill("#FF0000");
+	stroke("#FF0000");
+
+	if(scoreRightWrist > 0.2)
+	{ 
+		circle(rightWristX,rightWristY,20);
+
+			PeterPan.stop();
+
+		if(Harry_status == false)
+		{
+			HarryPotter.play();
+			document.getElementById("song").innerHTML = "Playing - Harry Potter Theme Song"
+		}
+	}
+	if(scoreLeftWrist > 0.2)
+	{
+		circle(leftWristX,leftWristY,20);
+
+			HarryPotter.stop();
+
+		if(Peter_status == false)
+		{
+			PeterPan.play();
+			document.getElementById("song").innerHTML = "Playing - Peter Pan Song"
+        }	
+    }
+}
+
